cb7ca8e802a7fca73e0655db9a7519c6ae7e0e81
Ehab-Tarek <ehabtarek166@gmail.com>
2023-09-17 00:43:26 +0300

    Add Footer.

--- Client/basic-banking-system/src/components/Footer/Footer.jsx
+++ Client/basic-banking-system/src/components/Footer/Footer.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <div>Footer</div>
+        <>
+            <div className='bg-dark text-white text-center p-2 fs-4'> © 2023 Irenaeus</div>
+        </>
     )
 }
